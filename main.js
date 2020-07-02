ae2523045ebb47cfc4faedb580bbbe5b406ce2a1
poby123 <youngsm12@naver.com>
2020-07-02 14:35:00 +0900

    Get File List

--- main.js
+++ main.js
@@ -2,39 +2,63 @@ var http = require('http');
 var fs = require('fs');
 var urlLib = require('url');
 
+function templateHTML(title, list, body){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>WEB1 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">WEB</a></h1>
+    ${list}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+
+function templateLIST(filelist){
+  var list = '<ul>';
+  for(var i = 0;i<filelist.length;i++){
+    list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+  }
+  list+='</ul>';
+  return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = urlLib.parse(_url, true).query; //get query from url
-    var title = queryData.id;
+    var pathname = urlLib.parse(_url, true).pathname;
 
-    if(_url == '/'){
-      title = 'Welcome';
+    if(pathname === '/'){
+      if(queryData.id === undefined){ //when welcome page
+        fs.readdir('./data', function(error, filelist){
+          var title = 'Welcome';
+          var description = 'Hello, Nodejs';
+          var list = templateLIST(filelist);
+          var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+          response.writeHead(200);
+          response.end(template);
+        });//fs.readdir end
+      }
+      else{ //when the other page
+        fs.readFile(`data/${queryData.id}`, 'utf8',function(err,description){
+          fs.readdir('./data', function(error, filelist){
+            var list = templateLIST(filelist);
+            var title = queryData.id;
+            var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+            response.writeHead(200);
+            response.end(template);
+          });//readdir end
+        });//readFile end
+      }//else end
+    }//if(pathname === '/') end
+    else{
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${queryData.id}`, 'utf8',function(err,description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
 });
 app.listen(3000);
